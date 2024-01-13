7bea18749dc7d3c71cf48369eb4addc5bd3273c2
KamilCiepluch <235845@eu.p.lodz.pl>
2024-01-13 15:30:54 +0100

    add endpoint to change space occupy status + add count endpoints

--- Backend/routes/parkingSpace.py
+++ Backend/routes/parkingSpace.py
@@ -12,6 +12,7 @@ def add_parking_space():
         parkingID = data.get("parkingID")
         spaceType = data.get("type")
         floor = data.get("floor")
+        spaceNumber = data.get("number")
 
         if spaceType.lower() not in acceptable_types:
             # todo check error number
@@ -21,7 +22,8 @@ def add_parking_space():
                 'parkingID': parkingID,
                 'type': spaceType,
                 'floor': floor,
-                'isTaken': False
+                'number': spaceNumber,
+                'isOccupied': False
             }
             db.collection("ParkingSpaces").add(jsonFile)
             return jsonify({"message": "Parking space added successfully."}), 200
@@ -30,6 +32,53 @@ def add_parking_space():
             return jsonify({"message": f"Error adding parking: {str(e)}"}), 500
 
     return jsonify({"message": "Invalid request method."}), 405
+
+
+@app.route("/change_parking_space_for_occupied", methods=["POST"])
+def change_space_status_for_occupied():
+
+    try:
+        data = request.json
+        parkingID = data.get("parkingID")
+        number = data.get("number")
+        floor = data.get("floor")
+
+        parking_spaces_ref = db.collection("ParkingSpaces").where("parkingID", "==", parkingID)\
+                                                           .where("number", "==", number) \
+                                                           .where("floor", "==", floor)
+        parking_spaces_snapshot = parking_spaces_ref.get()
+        if not parking_spaces_snapshot:
+            return jsonify({"message": "Parking space not found."}), 404
+
+        for doc in parking_spaces_snapshot:
+            doc.reference.update({"isOccupied": True})
+
+        return jsonify({"message": "Parking space occupied successfully."}), 200
+    except Exception as e:
+        return jsonify({"message": f"Error occupying parking space: {str(e)}"}), 500
+
+
+@app.route("/change_parking_space_for_unoccupied", methods=["POST"])
+def change_space_status_for_unoccupied():
+    try:
+        data = request.json
+        parkingID = data.get("parkingID")
+        number = data.get("number")
+        floor = data.get("floor")
+
+        parking_spaces_ref = db.collection("ParkingSpaces").where("parkingID", "==", parkingID) \
+            .where("number", "==", number) \
+            .where("floor", "==", floor)
+        parking_spaces_snapshot = parking_spaces_ref.get()
+        if not parking_spaces_snapshot:
+            return jsonify({"message": "Parking space not found."}), 404
+
+        for doc in parking_spaces_snapshot:
+            doc.reference.update({"isOccupied": False})
+
+        return jsonify({"message": "Parking space occupied successfully."}), 200
+    except Exception as e:
+        return jsonify({"message": f"Error occupying parking space: {str(e)}"}), 500
 
 
 @app.route("/all_parking_spaces", methods=["GET"])
@@ -58,14 +107,35 @@ def count_parking_spaces():
     parkingID = data.get("parkingID")
     spaceType = data.get("type")
     try:
-        parking_spaces_ref = db.collection("ParkingSpaces")\
-            .where("parkingID", "==", parkingID)\
-            .where("type", "==", spaceType)
-        parking_spaces_snapshot = parking_spaces_ref.get()
-        count = len(parking_spaces_snapshot)
+        if parkingID and spaceType:
+            parking_spaces_ref = db.collection("ParkingSpaces")\
+                .where("parkingID", "==", parkingID)\
+                .where("type", "==", spaceType)
+            parking_spaces_snapshot = parking_spaces_ref.get()
+            count = len(parking_spaces_snapshot)
 
-        return jsonify({"parking_id": parkingID, "space_type": spaceType, "count": count}), 200
+            return jsonify({"parking_id": parkingID, "space_type": spaceType, "count": count}), 200
+        return jsonify({"message": f"Missing arguments"}), 500
+    except Exception as e:
+        return jsonify({"message": f"Error counting parking spaces: {str(e)}"}), 500
 
+
+@app.route("/count_unoccupied_parking_spaces", methods=["GET"])
+def count_unoccupied_parking_spaces():
+    data = request.json
+    parkingID = data.get("parkingID")
+    spaceType = data.get("type")
+    try:
+        if parkingID and spaceType:
+            parking_spaces_ref = db.collection("ParkingSpaces")\
+                .where("parkingID", "==", parkingID)\
+                .where("type", "==", spaceType)\
+                .where("isOccupied", "==", False)
+            parking_spaces_snapshot = parking_spaces_ref.get()
+            count = len(parking_spaces_snapshot)
+
+            return jsonify({"parking_id": parkingID, "space_type": spaceType, "count": count}), 200
+        return jsonify({"message": f"Missing arguments"}), 500
     except Exception as e:
         return jsonify({"message": f"Error counting parking spaces: {str(e)}"}), 500
 
@@ -83,6 +153,28 @@ def count_parking_spaces_per_floor():
                 .where("parkingID", "==", parkingID)\
                 .where("floor", "==", floor)\
                 .where("type", "==", spaceType)
+            parking_spaces_snapshot = parking_spaces_ref.get()
+            count = len(parking_spaces_snapshot)
+            return jsonify({"parking_id": parkingID, "space_type": spaceType, "floor": floor, "count": count}), 200
+        return jsonify({"message": f"Missing arguments"}), 500
+
+    except Exception as e:
+        return jsonify({"message": f"Error counting parking spaces: {str(e)}"}), 500
+
+
+@app.route("/count_parking_unoccupied_spaces_per_floor", methods=["GET"])
+def count_parking_unoccupied_spaces_per_floor():
+    data = request.json
+    parkingID = data.get("parkingID")
+    spaceType = data.get("type")
+    floor = data.get("floor")
+    try:
+        if parkingID and spaceType and floor:
+            parking_spaces_ref = db.collection("ParkingSpaces")\
+                .where("parkingID", "==", parkingID)\
+                .where("floor", "==", floor)\
+                .where("type", "==", spaceType)\
+                .where("isOccupied", "==", False)
             parking_spaces_snapshot = parking_spaces_ref.get()
             count = len(parking_spaces_snapshot)
             return jsonify({"parking_id": parkingID, "space_type": spaceType, "floor": floor, "count": count}), 200
