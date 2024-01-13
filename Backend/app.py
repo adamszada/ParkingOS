ffca3010bce760c222aa87415838ce4970197a77
adamszada <235985@edu.p.lodz.pl>
2024-01-13 11:53:50 +0100

    Add get cars

--- Backend/app.py
+++ Backend/app.py
@@ -148,15 +148,13 @@ def update_exit_date(ticket_id):
 @app.route("/add_car", methods=["POST"])
 def add_car():
     if request.method == "POST":
-        data = request.json  # Get car data from the request
+        data = request.json  
 
-        # Extract car data from JSON
         brand = data.get('brand')
         model = data.get('model')
         license_plate = data.get('license_plate')
 
         try:
-            # Create a new document in the 'Cars' collection
             car_ref = db.collection('Cars').document()
             car_ref.set({
                 'brand': brand,
@@ -168,6 +166,24 @@ def add_car():
 
         except Exception as e:
             return jsonify({"message": f"Error adding car: {str(e)}"}), 500
+
+    return jsonify({"message": "Invalid request method."}), 405
+
+@app.route("/get_cars", methods=["GET"])
+def get_cars():
+    if request.method == "GET":
+        try:
+            cars_collection = db.collection('Cars').stream()
+            
+            cars_data = []
+            for car_doc in cars_collection:
+                car_data = car_doc.to_dict()
+                cars_data.append(car_data)
+
+            return jsonify({"cars": cars_data}), 200
+
+        except Exception as e:
+            return jsonify({"message": f"Error retrieving cars: {str(e)}"}), 500
 
     return jsonify({"message": "Invalid request method."}), 405
 
