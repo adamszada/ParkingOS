058b995b41a631990cd3df016fd7a5d7bf30e0a7
KamilCiepluch <235845@eu.p.lodz.pl>
2024-02-11 02:46:16 +0100

    add user endpoints

--- Backend/routes/users.py
+++ Backend/routes/users.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from app import app, db
-
+from firebase_admin import  auth
 
 @app.route("/get_users", methods=["GET"])
 def get_users():
@@ -19,3 +19,94 @@ def get_users():
             return jsonify({"message": f"Error retrieving cars: {str(e)}"}), 500
 
     return jsonify({"message": "Invalid request method."}), 405
+
+
+@app.route("/users", methods=["GET"])
+def get_all_users_with_balances():
+    try:
+        all_users = auth.list_users()
+        users_data = []
+
+        for user in all_users.users:
+            user_data = {
+                "uid": user.uid,
+                "email": user.email,
+                "saldo": get_user_balance(user.uid)  # Pobierz saldo użytkownika
+            }
+            users_data.append(user_data)
+
+        return jsonify({"users": users_data}), 200
+    except auth.AuthError as e:
+        return jsonify({"message": f"Error retrieving users: {str(e)}"}), 500
+
+
+@app.route("/topup_id", methods=["POST"])
+def top_up_balance_id():
+    try:
+        # Pobierz dane z żądania
+        user_id = request.json.get('userId')
+        amount = request.json.get('amount')
+
+        if not user_id or not amount:
+            return jsonify({"message": "User ID and amount are required."}), 400
+
+        # Sprawdź czy użytkownik istnieje
+        user = auth.get_user(user_id)
+
+        # Pobierz aktualne saldo użytkownika
+        current_balance = get_user_balance(user_id)
+
+        # Zwiększ saldo o podaną kwotę
+        new_balance = current_balance + amount
+
+        # Zapisz nowe saldo w profilu użytkownika
+        auth.set_custom_user_claims(user_id, {"saldo": new_balance})
+
+        return jsonify({"message": f"Successfully topped up user balance. New balance: {new_balance}"}), 200
+    except auth.AuthError as e:
+        return jsonify({"message": f"Error topping up user balance: {str(e)}"}), 500
+
+
+@app.route("/topup", methods=["POST"])
+def top_up_balance():
+    try:
+        # Pobierz dane z żądania
+        email = request.json.get('email')
+        amount = request.json.get('amount')
+
+        if not email or not amount:
+            return jsonify({"message": "Email and amount are required."}), 400
+
+        # Sprawdź czy użytkownik istnieje
+        user = auth.get_user_by_email(email)
+
+        # Pobierz UID użytkownika
+        user_id = user.uid
+
+        # Pobierz aktualne saldo użytkownika
+        current_balance = get_user_balance(user_id)
+
+        # Zwiększ saldo o podaną kwotę
+        new_balance = current_balance + amount
+
+        # Zapisz nowe saldo w profilu użytkownika
+        auth.set_custom_user_claims(user_id, {"saldo": new_balance})
+
+        return jsonify({"message": f"Successfully topped up user balance. New balance: {new_balance}"}), 200
+    except auth.AuthError as e:
+        return jsonify({"message": f"Error topping up user balance: {str(e)}"}), 500
+
+
+def get_user_balance(uid):
+    try:
+        # Pobierz użytkownika na podstawie UID
+        user = auth.get_user(uid)
+        custom_claims = user.custom_claims
+        if custom_claims and "saldo" in custom_claims:
+            saldo = custom_claims["saldo"]
+            return saldo
+        else:
+            return 0  #
+    except auth.AuthError as e:
+        print(f"Error retrieving user balance: {str(e)}")
+        return 0  # W przypadku błędu, zwróć 0
