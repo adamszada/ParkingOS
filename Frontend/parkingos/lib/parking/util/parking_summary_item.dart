cc332f0ec662b136075fcbcdfafd68e572f96d7f
Jakub Bernaciak <bernaciak.jm@gmail.com>
2024-02-12 20:02:23 +0100

    Add summary page

---
+++ Frontend/parkingos/lib/parking/util/parking_summary_item.dart
@@ -0,0 +1,12 @@
+class ParkingSummaryItem {
+  late String month = "";
+  late String year = "";
+  late double costs;
+  late double earning;
+  ParkingSummaryItem({
+    required this.month,
+    required this.year,
+    required this.costs,
+    required this.earning,
+  });
+}
